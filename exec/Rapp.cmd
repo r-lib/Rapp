076b48c4e68e9c9b1b8ea2a6d40d1e4e8a17578d
Tomasz Kalinowski <kalinowskit@gmail.com>
2023-03-17 14:23:17 -0400

    Fix Rapp on Windows

--- exec/Rapp.cmd
+++ exec/Rapp.cmd
@@ -1,1 +1,18 @@
-@Rscript --vanilla -e Rapp::run() %*
+@echo off
+setlocal
+
+set filename=%1
+
+for /f "delims=" %%a in ('where /f %filename%') do (
+    set filepath=%%a
+    goto :run
+)
+
+set 
+echo File not found on PATH: %filename%
+exit /b 1
+
+@REM No straighforward way to pop off first arg from %*
+@REM So we do it in R
+:run
+Rscript.exe -e Rapp::run(args=commandArgs(TRUE)[-2L]) %filepath% %*
